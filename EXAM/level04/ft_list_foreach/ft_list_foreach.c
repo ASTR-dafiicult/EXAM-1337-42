d5d78eb0705fc770516691f13ad8e6e6f79d38f2
IONYX-YOURI <hamzaofficer2@gmail.com>
2026-01-31 00:22:58 +0100

    Update ft_list_foreach.c

--- EXAM/level04/ft_list_foreach/ft_list_foreach.c
+++ EXAM/level04/ft_list_foreach/ft_list_foreach.c
@@ -1,10 +1,11 @@
 //# include <stdio.h>
 # include "ft_list.h"
-
+/*
 void f(void *data)
 {
     printf("%d\n", *(int *)data);
 }
+*/
 void    ft_list_foreach(t_list *begin_list, void (*f)(void *))
 {
     t_list *current;
