34c2435451daaa240dacf326e5cf7f692bf81811
IONYX-YOURI <hamzaofficer2@gmail.com>
2026-02-04 17:54:15 +0100

    Refactor hex printing and remove unused code

--- EXAM/level03/print_hex/print_hex.c
+++ EXAM/level03/print_hex/print_hex.c
@@ -1,6 +1,7 @@
 # include <unistd.h>
 
 
+
 void ft_putchar(char c)
 {
     write(1, &c, 1);
@@ -8,55 +9,49 @@ void ft_putchar(char c)
 int ft_atoi(char *str)
 {
     int res = 0;
-    int sign = 1;
     int i = 0;
     while(str[i] == 32 || str[i] == 9)
-    {
         i++;
-    }
-    while(str[i] == 45 || str[i] == 43)
-    {
-        if(str[i] == 45)
-        {
-            sign *= -1;
-        }
-        i++;
-    }
     while(str[i] >= '0' && str[i] <= '9')
     {
         res = res * 10 + str[i] - 48;
         i++;
     }
-    return res * sign;
-}
-void print_hex(unsigned int nb)
-{
-    char hex[] = "0123456789abcdef";
-    char tab[16];
-    int i = 0;
-    while(nb)
-    {
-        tab[i] = hex[nb % 16];
-        nb /= 16;
-        i++;
-    }
-    while(i >= 0)
-    {
-        ft_putchar(tab[i]);
-        i--;
-    }
+    return res;
 }
 int main(int ac, char **av)
 {
     if(ac == 2)
     {
-        int val1 = ft_atoi(av[1]);
-        if(val1 < 0)
+        int nb = ft_atoi(av[1]);
+        int size = 0;
+        int i = 0;
+        char hex[] = "0123456789abcdef";
+        int tmp = nb;
+        
+        if(nb == 0)
         {
-            write(1, "\n", 1);
+            ft_putchar('0');
+            ft_putchar('\n');
             return 0;
         }
-        print_hex(val1);
+        while(tmp)
+        {
+            tmp /= 16;
+            size++;
+        }
+        char str[size];
+        str[size] = '\0';
+        i = size - 1;
+        while(nb)
+        {
+            str[i--] = hex[nb % 16];
+            nb /= 16;
+        }
+        i = 0;
+        while(str[i])
+            ft_putchar(str[i++]);
     }
-    write(1, "\n", 1);
+    ft_putchar('\n');
+    return 0;
 }
