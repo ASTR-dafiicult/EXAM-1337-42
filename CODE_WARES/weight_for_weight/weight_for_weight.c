a161c404030ebe63fb089d0ffc1d33fd1bc12320
IONYX-YOURI <hamzaofficer2@gmail.com>
2026-02-24 02:12:52 +0100

    Refactor weight_for_weight.c with new functions

--- CODE_WARES/weight_for_weight/weight_for_weight.c
+++ CODE_WARES/weight_for_weight/weight_for_weight.c
@@ -1,152 +1,154 @@
-#include <stdlib.h>
-# include <stdio.h>
-# include <string.h>
+# include <unistd.h>
+# include <stdlib.h>
 
+
+
+//check space
+int blank(char c)
+{
+	if(c == 32 || c == 9)
+		return 1;
+	return 0;
+}
+//swap by order
 void swap(char **a, char **b)
 {
-    char *tmp;
-
-    tmp = *a;
-    *a = *b;
-    *b = tmp;
+	char *tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
-int weight(char *s)
+//calcule sum of number n100 == 1n
+int convert_sum(char *str)
 {
-    int sum = 0;
-    int i = 0;
-
-    while (s[i])
-    {
-        if (s[i] >= '0' && s[i] <= '9')
-            sum += s[i] - '0';
-        i++;
-    }
-    return sum;
+	int i = 0;
+	int sum = 0;
+	while(str[i])
+	{
+		sum += str[i] - 48;
+		i++;
+	}
+	return sum;
 }
-int compare(char *a, char *b)
+//cump the two values
+int cmp_sum(char *s1, char *s2)
 {
-    int wa = weight(a);
-    int wb = weight(b);
-
-    if (wa < wb)
-        return -1;
-    if (wa > wb)
-        return 1;
-    return strcmp(a, b);
+	int w1 = convert_sum(s1);
+	int w2 = convert_sum(s2);
+	if(w1 > w2)
+		return 1;
+	if(w1 <= w2)
+		return -1;
 }
-int space(char c)
+//count how many string in one string
+int count_word(const char *str)
 {
-    if(c == 32 || c == 9)
-        return 1;
-    return 0;
+	int index = 0;
+	int i = 0;
+	int val;
+	while(str[i])
+	{
+		val = blank(str[i]);
+		if(!val)
+		{
+			if(i == 0 || blank(str[i - 1]))
+				index++;
+		}
+		i++;
+	}
+	return index;
 }
-int count_words(const char *str)
+//count length of every string
+int length_string(const char *tab)
 {
-    int count = 0;
-    int i = 0;
-    while(str[i])
-    {
-        if(!space(str[i]))
-        {
-            if(i == 0 || space(str[i - 1]))
-            {
-                count++;
-            }
-        }
-        i++;
-    }
-    return count;
+	int i = 0;
+	while(tab[i] && !blank(tab[i]))
+		i++;
+	return i;
 }
-int length_word(const char *word)
+//copy string to another string 2D
+char *copy(const char *tab, int length)
 {
-    int i = 0;
-    while(word[i] && !space(word[i]))
-    {
-        i++;
-    }
-    return i;
+	char *str = malloc((length + 1) * sizeof(char));
+	if(!str)
+		return NULL;
+	int i = 0;
+	while(i < length)
+	{
+		str[i] = tab[i];
+		i++;
+	}
+	str[i] = '\0';
+	return str;
 }
-char *copy(const char *word, int length)
-{
-    int i = 0;
-    char *str = malloc((length + 1) * sizeof(char));
-    if(!str)
-        return NULL;
-    while(i < length)
-    {
-        str[i] = word[i];
-        i++;
-    }
-    str[i] = '\0';
-    return str;
-}
+//function 1er
 char *orderWeight(const char *string)
 {
-    int len = 0;
-    while(string[len])
-        len++;   
+	 int len = 0;
+         while(string[len])
+                 len++;
+	int size = count_word(string);
+	char **tab = malloc((size + 1) * sizeof(char *));
+	if(!tab)
+		return NULL;
+	int index = 0;
+	int length;
+	int i = 0;
+	while(string[i])
+	{
+		if(!blank(string[i]))
+		{
+			length = length_string(&string[i]);
+			tab[index++] = copy(&string[i], length);
+			i += length;
+		}
+		else
+			i++;
+	}
+	tab[index] = NULL;
+	i = 0;
+	int j;
+	while(tab[i])
+	{
 
-    int size = count_words(string);
-    char **str = malloc((size + 1) * sizeof(char*));
-    int i = 0;
-    int index = 0;
-    while(string[i])
-    {
-        if(!space(string[i]))
-        {
-            int length = length_word(&string[i]);
-            str[index] = copy(&string[i], length);
-            index++;
-            i += length;
-        }
-        else
-            i++;
-    }
-    str[index] = NULL;
-
-    i = 0;
-    int k;
-    while (str[i])
-    {
-        k = i + 1;
-        while (str[k])
-        {
-            if (compare(str[i], str[k]) > 0)
-                swap(&str[i], &str[k]);
-            k++;
-        }
-        i++;
-    }
-    i = 0;
-    int j;
-    index = 0;
-    char *tab = malloc((len + 1) * sizeof(char));
-    while(str[i])
-    {
-        j = 0;
-        while(str[i][j])
-        {
-            tab[index++] = str[i][j];
-            j++;
-        }
-        i++;
-        if(str[i] != NULL)
-        {
-            tab[index++] = ' ';
-        }
-    }
-    tab[index] = '\0';
-    return tab;
+		j = i + 1;
+		while(tab[j])
+		{
+			if(cmp_sum(tab[i], tab[j]) > 0)
+				swap(&tab[i], &tab[j]);
+			j++;
+		}
+		i++;
+	}
+	char *str = calloc((len + 1), sizeof(char));
+	if(!str)
+		return NULL;
+	i = 0;
+	index = 0;
+	while(tab[i])
+	{
+		j = 0;
+		while(tab[i][j])
+			str[index++] = tab[i][j++];
+		i++;
+		if(tab[i] != NULL)
+			str[index++] = ' ';
+	}
+	str[index] = '\0';
+	i = 0;
+	while(tab[i])
+		free(tab[i++]);
+	free(tab);
+	return str;
 }
+# include <stdio.h>
 int main()
 {
-    char str[] = "103 123 4444 99 2000";
-    char *tab = orderWeight(str);
-    int i = 0;
-    while(tab[i])
-    {
-        printf("%c", tab[i]);
-        i++;
-    }
-    printf("\n");
+	const char *str = "100 20 97 99 56";
+	char *src = orderWeight(str);
+	int i = 0;
+	while(src[i])
+		printf("%c", src[i++]);
+	printf("\n");
+
+	free(src);
 }
