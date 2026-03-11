566cb45795843fa1d95c34354ae205da624b74d7
IONYX-YOURI <hamzaofficer2@gmail.com>
2026-03-11 04:06:38 +0100

    Update n_queens_puzzle.c

--- RECURSION_BACKTRACKING/N_queens_puzzle/n_queens_puzzle.c
+++ RECURSION_BACKTRACKING/N_queens_puzzle/n_queens_puzzle.c
@@ -1,5 +1,17 @@
 # include <unistd.h>
 
+int ft_atoi(char *str)
+{
+    int i = 0;
+    int res = 0;
+    while(str[i] == 32 || str[i] == 9)
+        i++;
+    while(str[i])
+    {
+        res = res * 10 + str[i++] - 48;
+    }
+    return res;
+}
 void ft_putchar(char c)
 {
     write(1, &c, 1);
@@ -50,7 +62,7 @@ int solve_n_queen(int n, int Q[n], int row, int *counter)
     int col = 0;
     while(col < n)
     {
-        if(isSafe(n, Q, row, col))//choice //recursion //backtrack
+        if(isSafe(n, Q, row, col))
         {
             Q[row] = col;
             solve_n_queen(n , Q, row + 1, counter);
@@ -59,19 +71,26 @@ int solve_n_queen(int n, int Q[n], int row, int *counter)
     }
     return *counter;
 }
-void N_queens_problem()
+void N_queens_problem(int n)
 {
     int row = 0;
-    int n = 4;
     int Q[n];
-    int count = solve_n_queen(n, Q, row, &count);
+    int count = 0;
+    count = solve_n_queen(n, Q, row, &count);
     ft_putnbr(count);
 }
-/*
-int main()
+int main(int ac, char **av)
 {
-    N_queens_problem();
+    if(ac == 2)
+    {
+        int nb = ft_atoi(av[1]);
+        if(nb <= 0)
+        {
+            ft_putnbr(0);
+            ft_putchar('\n');
+            return 0;
+        }
+        N_queens_problem(nb);
+    }
     ft_putchar('\n');
 }
-*/
-
